fbd8055a5f31b74fb13817eed0a7795552e3a12a
Capfund <onions193@gmail.com>
2026-01-16 13:54:55 +0700

    use os.system() to compile to a.out with nasm and gcc

--- src/test.py
+++ src/test.py
@@ -2,6 +2,7 @@ from lexer.lexer import Lexer
 from parser.parser import Parser
 from semantic import SemanticAnalyzer
 from compiler.x86_64_linux import x86_64_Linux
+import os
 
 source = """
 int factorial(int n) {
@@ -31,3 +32,6 @@ print(asm)
 
 with open("out.asm", "w") as f:
     f.write(asm)
+
+os.system("nasm -felf64 out.asm")
+os.system("gcc out.o -no-pie")
